5983ec41af9dd13ba25c5153ef87796c3946cca4
Chris Chapman <christopher.chapman@me.com>
2013-07-09 05:01:24 -0400

    API Proof of concept implimented

---
+++ inc/js/main.js
@@ -0,0 +1,71 @@
+angular.module('wallpaper', [], function($httpProvider){
+	// Override json posts. Full details - http://victorblog.com/2012/12/20/make-angularjs-http-service-behave-like-jquery-ajax/
+  // Use x-www-form-urlencoded Content-Type
+  $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+ 
+  // Override $http service's default transformRequest
+  $httpProvider.defaults.transformRequest = [function(data)
+  {
+    /**
+     * The workhorse; converts an object to x-www-form-urlencoded serialization.
+     * @param {Object} obj
+     * @return {String}
+     */ 
+    var param = function(obj)
+    {
+      var query = '';
+      var name, value, fullSubName, subName, subValue, innerObj, i;
+      
+      for(name in obj)
+      {
+        value = obj[name];
+        
+        if(value instanceof Array)
+        {
+          for(i=0; i<value.length; ++i)
+          {
+            subValue = value[i];
+            fullSubName = name + '[' + i + ']';
+            innerObj = {};
+            innerObj[fullSubName] = subValue;
+            query += param(innerObj) + '&';
+          }
+        }
+        else if(value instanceof Object)
+        {
+          for(subName in value)
+          {
+            subValue = value[subName];
+            fullSubName = name + '[' + subName + ']';
+            innerObj = {};
+            innerObj[fullSubName] = subValue;
+            query += param(innerObj) + '&';
+          }
+        }
+        else if(value !== undefined && value !== null)
+        {
+          query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+        }
+      }
+      
+      return query.length ? query.substr(0, query.length - 1) : query;
+    };
+    
+    return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+  }];
+});
+
+
+function Reddit($scope, $http) {
+
+	var postData = {'url' : 'http://www.reddit.com/r/wallpapers.json'};
+
+	$http({method: 'POST', url: 'inc/php/cross.php', data: postData}).
+	success(function(data, status, headers, config) {
+		console.log('success', data);
+		$scope.wallpapers = data.data.children;
+	}).
+	error(function(data, status, headers, config) {
+		console.log('error', data);
+	});
+};
---
+++ inc/php/cross.php
@@ -0,0 +1,50 @@
+<?php
+/**
+  crossDomain
+ 
+  cURL's out to a supplied URL while passing the $_REQUEST global onto the 
+  endpoint allowing all supplied data to submit properly.
+
+  This is a simple utility script that could be improved upon, don't be 
+  afraid to do so.
+
+  @author Chris Chapman - twitter - @_chapman
+  @param  (str)$url A valid URL should be provided
+  @return (ary) or (false)
+ */
+
+function crossDomain($url = ''){
+	$urlPattern = "~^(https?|ftp)://www\.([a-z\d.-]+(:[a-z\d.&%$-]+)*@)*((25[0-5]|2[0-4]\d|[0-1]\d{2}|[1-9]\d|[1-9])\.(25[0-5]|2[0-4]\d|[0-1]\d{2}|[1-9]\d|[1-9]|0)\.(25[0-5]|2[0-4]\d|[0-1]\d{2}|[1-9]\d|[1-9]|0)\.(25[0-5]|2[0-4]\d|[0-1]\d{2}|[1-9]\d|\d)|([a-z\d-]+\.)+(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-z]{2}))(:\d+)*(/($|[\w.,?'\\+&%$#=\~-]+))*$~i";
+	$validURL = preg_match($urlPattern, $url);
+	if(!$validURL) return false;
+	$data 			= $_REQUEST;
+	//$data_string	= json_encode($data);
+
+	// instance a new cURL
+	$ch = curl_init();
+
+	// configure the cURL
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HEADER,0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+	// execute the cURL
+	$result = curl_exec($ch);
+
+	// close cURL
+	curl_close($ch);
+
+	return $result;
+}
+	
+// Call crossDomain. If an error occured, force an error msg into $data;
+if(!$data = crossDomain( $_REQUEST['url'] )){
+	$data = json_encode( array('Error' => 'Request did not succeed.' ) );
+
+} 
+
+// We are about to echo out some json text, so we should set the content type of this file
+header('Content-Type: application/json');
+
+// display the content in a json format
+echo $data;
